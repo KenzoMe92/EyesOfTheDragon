8f23e4de839de596bdb137de0f234ce72bdf55ec
Mario Zizzo <kenzome92@outlook.com>
2024-04-04 21:33:41 +0200

    Began working on ControlManager

---
+++ SharedProject/Controls/ControlManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SharedProject.Controls
+{
+    public class ControlManager : List<Control>
+    {
+        #region Fields and Properties
+
+        int selectedControl = -1;
+        static SpriteFont spriteFont;
+
+        public static SpriteFont SpriteFont
+        {
+            get { return spriteFont; }
+        }
+
+        #endregion
+    }
+}
